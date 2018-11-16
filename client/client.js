93d3d1ffffad3a45b72ced832899781bf42af8dd
Alvaro Sebastián Galindo Calderón <sebastian28.gc@gmail.com>
2018-11-15 20:26:42 -0600

    Laboratorio 5

---
+++ client/client.js
@@ -0,0 +1,85 @@
+
+var socket = io.connect('http://localhost:3000');
+const url = 'http://34.210.35.174:7000/';
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+function sendMessage(){
+  const input = document.getElementById('message-input').value;
+  if (input == ""){
+    //Window alert if there is no text in the input field.
+    window.alert("Por favor, ingresa un mensaje")
+  }else{
+    socket.emit('chat message', {
+      student_id : '15452',
+      text : input,
+      nick : 'King'
+    });
+  }
+}
+
+
+Retrieveing all server messages with the fetch API
+fetch(url)
+	.then((resp) => resp.json()) // Transform the data into json
+ 	.then((data) => {
+    	let messages = data;
+    	return messages.map((message) => {
+        li_cont++;
+    		let li = createNode("li"),
+    			text = document.createTextNode(message.nick + ": " + message.text);
+			if (message.nick != "" && message.text != "" ) {
+				li.append(text);
+    		ul.append(li);
+        if(message.student_id == "15452"){
+          li.className = "own_message"
+        }
+			}
+        objDiv.scrollTop = objDiv.scrollHeight;
+    	})
+//     })
+
+
+// Get the input field
+var input = document.getElementById("message-input");
+//Creating event for send button
+const submitBtn = document.querySelector('#sendButton');
+submitBtn.addEventListener('click', function(){
+  sendMessage();
+  input.value="";
+});
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("sendButton").click();
+    input.value="";
+  }
+});
+
+//Socket on to check for incoming messages
+socket.on('chat message', function(msg){
+  console.log("Message received from ", msg.nick);
+  const ul = document.getElementById('messages'); // Get the list where we will place our messages
+  if(msg.nick != "" && msg.text != ""){
+    text = document.createTextNode(msg.nick + ": " + msg.text);
+    let li = createNode("li");
+    if(msg.student_id == "15452"){
+      li.className = "own_message"
+    }
+    var audio = new Audio('discord_message.mp3');
+    audio.play();
+    li.append(text);
+    ul.append(li);
+  }
+});
---
+++ server/server.js
@@ -0,0 +1,92 @@
+var li_cont = 0;
+var li_server_cont=0;
+let messages = null;
+const url = 'http://34.210.35.174:7000/';
+const fetch = require("node-fetch");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+function getMessages(){
+  const url = 'http://34.210.35.174:7000/';
+  fetch(url)
+  	.then((resp) => resp.json()) // Transform the data into json
+   	.then((data) => {
+      	messages = data;
+        li_server_cont=0;
+        messages.forEach(function(element){
+          li_server_cont++;
+        });
+      })
+}
+
+function updateMessages(){
+  getMessages();
+  if(li_server_cont>li_cont){
+    var delta = li_server_cont - li_cont;
+    for (var i = 0; i < delta; i++) {
+      if (messages[li_server_cont-i-1].nick  != "" && messages[li_server_cont-i-1].text != "" ) {
+				console.log("Este es el Id: ", messages[li_server_cont-i-1].student_id )
+				io.emit('chat message', {
+					student_id: messages[li_server_cont-i-1].student_id,
+					text: messages[li_server_cont-i-1].text,
+					nick: messages[li_server_cont-i-1].nick
+				});
+      }
+    }
+      li_cont = li_server_cont;
+  }
+}
+
+//Retrieveing all server messages with the fetch API
+console.log("Making fetch...")
+fetch(url)
+	.then((resp) => resp.json()) // Transform the data into json
+ 	.then((data) => {
+    	let messages = data;
+    	return messages.map((message) => {
+        li_cont++;
+				if (message.nick != "" && message.text != "" ) {
+					console.log("Message from ", message.student_id, ", says: ", message.text)
+					io.emit('chat message', {
+						student_id: message.student_id,
+						text: message.text,
+						nick: message.nick
+					});
+				}
+    	})
+    })
+
+http.listen(3000, function(){
+  console.log('listening...');
+});
+
+io.on('connection', function(socket) {
+	socket.on('chat message', function(msg){
+    console.log('message: ' + msg.nick);
+		//data going to send in the request.
+		const data = new FormData();
+		data.append('student_id', msg.student_id);
+		data.append('text', msg.text);
+		data.append('nick', msg.nick);
+
+		//Creating fetchData
+		let fetchData = {
+			method: 'POST',
+			body : data
+		}
+		fetch(url, fetchData)
+		.then(function (data) {
+			console.log('Request success: ', data);
+			getMessages();
+		})
+		.catch(function (error) {
+			console.log('Request failure: ', error);
+		});
+  });
+	setInterval(function() {
+	    // method to be executed;
+			console.log('Updating messages');
+	    updateMessages()
+	  }, 1000);
+});
